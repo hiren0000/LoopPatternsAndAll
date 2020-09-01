16959cac308d28427b0a8ed7601cfbda2299f1e1
hiren0000 <rebeldevmurari@gmail.com>
2020-09-01 23:54:28 +0200

    created some loop patterns

---
+++ LoopPatternsAndLoops/src/com/rebel1/LoopPattern.java
@@ -0,0 +1,47 @@
+package com.rebel1;
+
+public class LoopPattern {
+
+	
+	
+	
+	public static void main(String[] args) {
+	
+		LoopPattern lp = new LoopPattern();
+	
+		
+		
+		// Nested loop
+		for(int j=1;j<=5;j++)      // rows 
+		{
+		  
+			for(int i=1;i<=j;i++)   //column this loop will start from 1 whenever it evokes again
+		{
+			System.out.print(i);
+			
+		}
+			System.out.println("");
+		}
+		
+		System.out.println("");
+	 
+		  lp.loop();
+		  System.out.println("");
+	}
+		
+	public void loop()
+	{
+		for(int k=5;k>=1;k--)
+		{
+			for(int l=k;l>=1;l--)
+			{
+				System.out.print(l);
+			}
+			System.out.println();
+		}
+	}
+	
+
+}
+
+
